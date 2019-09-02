c03af2089791889d9309c58c60aa20443b954760
Manu <manu.scazza@hotmail.it>
2019-09-02 14:49:39 +0200

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Navbar from 'react-bootsrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Navbar bg="light" expand="lg">
+   <Navbar.Brand href="#home">BookME</Navbar.Brand>
+   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+   <Navbar.Collapse id="basic-navbar-nav">
+     <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+       <Nav.Link href="#link">Users</Nav.Link>
+       <Nav.Link href="#link">Stories</Nav.Link>
+       <NavDropdown title="Categories" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+         <NavDropdown.Item href="#action/3.2">Sci-fi</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Romance</NavDropdown.Item>
+       </NavDropdown>
+     </Nav>
+     <Form inline>
+       {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
+       <Button variant="outline-success">Logout</Button>
+     </Form>
+   </Navbar.Collapse>
+ </Navbar>
+ </header>
+</div>
+
+
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './Login.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+// function Login() {
+//     return (
+//         <div className="Login">
+//             <h1 id="content">Login</h1>
+//             <Form>
+//   <Form.Group controlId="formBasicUsername">
+//     <Form.Label>Username</Form.Label>
+//     <Form.Control type="text" placeholder="Enter username" />
+//   </Form.Group>
+
+//   <Form.Group controlId="formBasicPassword">
+//     <Form.Label>Password</Form.Label>
+//     <Form.Control type="password" placeholder="Password" />
+//   </Form.Group>
+//   <Button variant="primary" type="submit">
+//     Submit
+//   </Button>
+// </Form>
+//         </div>
+//     );
+// }
+
+// export default Login;
+
+export default class Login extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        username: '',
+        password: ''
+      };
+    }
+    handleInputChange = (event) => {
+      const { value, name } = event.target;
+      this.setState({
+        [name]: value
+      });
+    }
+    onSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://localhost:8080/api/authenticate', {
+          method: 'POST',
+          body: JSON.stringify(this.state),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(res => {
+            console.log(res);
+          if (res.status === 200) {
+            //this.props.history.push('/');
+          } else {
+            const error = new Error(res.error);
+            throw error;
+          }
+        })
+        .catch(err => {
+          console.error(err);
+          alert('Error logging in please try again');
+        });
+      }
+    render() {
+      return (
+        <form className="Login" onSubmit={this.onSubmit}>
+          <h1 id="content">Login</h1>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            required
+          />
+         <Button variant="primary" type="submit">
+                Submit
+        </Button>
+        </form>
+      );
+    }
+  }
